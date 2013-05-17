d1ad0b67b681aead74ad67332d5e1a696cabe653
Sen Wang <moli.wang@gmail.com>
2013-05-17 17:14:53 +0800

    use AppChoosesSkeletons and ChooseSkeletons to choose specific skeleton

--- SoulSolutions.Kinect.SampleApplication/MainWindow.xaml.cs
+++ SoulSolutions.Kinect.SampleApplication/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System;
 using System.Linq;
 using System.Windows;
 using Microsoft.Kinect;
+using System.Collections.Generic;
 
 #endregion
 
@@ -50,7 +51,7 @@ namespace SoulSolutions.Kinect.SampleApplication
         }
 
 
-        //只選用離感應器最近的使用者顯示其骨架
+        //停止自動選擇骨架功能，由開發者自己選擇要追蹤的骨架
         private void NuiSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
         {
             SkeletonFrame allSkeletons = e.OpenSkeletonFrame();
@@ -59,39 +60,79 @@ namespace SoulSolutions.Kinect.SampleApplication
             {
                 if (skframe != null)
                 {
+                    #region enable app choose skleltons
+                    if (!nui.SkeletonStream.AppChoosesSkeletons)
+                    {
+                        nui.SkeletonStream.AppChoosesSkeletons = true;
+                    }
+                    #endregion
+
                     Skeleton[] FrameSkeletons = new Skeleton[skframe.SkeletonArrayLength];
                     skframe.CopySkeletonDataTo(FrameSkeletons);
 
-                    #region user count
-                    var nonactiveuser = from s in FrameSkeletons
-                                     where s.TrackingState != SkeletonTrackingState.Tracked
-                                     select s;
-                    var activeuser = from s in FrameSkeletons
-                                     where s.TrackingState == SkeletonTrackingState.Tracked
+                    var alluser = from s in FrameSkeletons
+                                     where s.TrackingState != SkeletonTrackingState.NotTracked
                                      select s;
 
-                    int users = activeuser.Count() ;
-                    int nonusers = nonactiveuser.Count();
-                    Title = "目前追蹤到的骨架數量為 : " + users + "  尚未骨架數量為 : " + nonusers;
-                    #endregion
-
-                    if (users == 1)
+                    int allusers = alluser.Count() ;
+                    Title = "所有可追蹤到的骨架數量為 : " + allusers ;
+                    if (allusers > 0)
                     {
-                        skeleton1.SkeletonData = activeuser.ElementAt(0);
-                    }
-                    else if (users == 2)
-                    {
-                        var dist1 = activeuser.ElementAt(0).Position.Z;
-                        var dist2 = activeuser.ElementAt(1).Position.Z;
-                        if( dist1 < dist2)
-                            skeleton1.SkeletonData = activeuser.ElementAt(0);
-                        else
-                            skeleton1.SkeletonData = activeuser.ElementAt(1);
+                        Skeleton user = ClosestUser(alluser);
+                        nui.SkeletonStream.ChooseSkeletons(user.TrackingId);
+                        skeleton1.SkeletonData = user;
                     }
                 }
             }           
         }
+        private Skeleton ClosestUser(IEnumerable<Skeleton> users)
+        {
+            float mindist = users.Min( s => s.Position.Z) ;
+            return users.FirstOrDefault( s => s.Position.Z == mindist);
+        }
 
+
+        ////只選用離感應器最近的使用者顯示其骨架
+        //private void NuiSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        //{
+        //    SkeletonFrame allSkeletons = e.OpenSkeletonFrame();
+
+        //    using (SkeletonFrame skframe = e.OpenSkeletonFrame())
+        //    {
+        //        if (skframe != null)
+        //        {
+        //            Skeleton[] FrameSkeletons = new Skeleton[skframe.SkeletonArrayLength];
+        //            skframe.CopySkeletonDataTo(FrameSkeletons);
+
+        //            #region user count
+        //            var nonactiveuser = from s in FrameSkeletons
+        //                                where s.TrackingState != SkeletonTrackingState.Tracked
+        //                                select s;
+        //            var activeuser = from s in FrameSkeletons
+        //                             where s.TrackingState == SkeletonTrackingState.Tracked
+        //                             select s;
+
+        //            int users = activeuser.Count();
+        //            int nonusers = nonactiveuser.Count();
+        //            Title = "目前追蹤到的骨架數量為 : " + users + "  尚未骨架數量為 : " + nonusers;
+        //            #endregion
+
+        //            if (users == 1)
+        //            {
+        //                skeleton1.SkeletonData = activeuser.ElementAt(0);
+        //            }
+        //            else if (users == 2)
+        //            {
+        //                var dist1 = activeuser.ElementAt(0).Position.Z;
+        //                var dist2 = activeuser.ElementAt(1).Position.Z;
+        //                if (dist1 < dist2)
+        //                    skeleton1.SkeletonData = activeuser.ElementAt(0);
+        //                else
+        //                    skeleton1.SkeletonData = activeuser.ElementAt(1);
+        //            }
+        //        }
+        //    }
+        //}
 
         ////使用 LINQ語法篩選出所有已被追蹤之骨架，分別對不同的已追蹤骨架給予不同的骨架資訊
         //private void NuiSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
